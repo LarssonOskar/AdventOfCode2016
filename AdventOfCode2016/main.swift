1f74193b448cae9067eda9f9132c4f57f99216da
Oskar Larsson <oskar.larsson91@gmail.com>
2016-12-01 21:43:12 +0100

    Initial Commit

---
+++ AdventOfCode2016/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2016
+//
+//  Created by Oskar Larsson on 2016-12-01.
+//  Copyright © 2016 Oskar Larsson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
