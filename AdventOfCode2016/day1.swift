c4c402e46d3f9d75c42d30aa3a24507b36ec9c82
Oskar Larsson <oskar.larsson91@gmail.com>
2016-12-02 01:20:25 +0100

    Cleanup

--- AdventOfCode2016/day1.swift
+++ AdventOfCode2016/day1.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 class Day1 {
-
+    
     func start() -> Int {
         
         let input = "R5, R4, R2, L3, R1, R1, L4, L5, R3, L1, L1, R4, L2, R1, R4, R4, L2, L2, R4, L4, R1, R3, L3, L1, L2, R1, R5, L5, L1, L1, R3, R5, L1, R4, L5, R5, R1, L185, R4, L1, R51, R3, L2, R78, R1, L4, R188, R1, L5, R5, R2, R3, L5, R3, R4, L1, R2, R2, L4, L4, L5, R5, R4, L4, R2, L5, R2, L1, L4, R4, L4, R2, L3, L4, R2, L3, R3, R2, L2, L3, R4, R3, R1, L4, L2, L5, R4, R4, L1, R1, L5, L1, R3, R1, L2, R1, R1, R3, L4, L1, L3, R2, R4, R2, L2, R1, L5, R3, L3, R3, L1, R4, L3, L3, R4, L2, L1, L3, R2, R3, L2, L1, R4, L3, L5, L2, L4, R1, L4, L4, R3, R5, L4, L1, L1, R4, L2, R5, R1, R1, R2, R1, R5, L1, L3, L5, R2";
@@ -23,7 +23,7 @@ class Day1 {
             .components(separatedBy: ",");
         var endPosition = routeCoordinates();
         for ins in instructions {
-            var instruction = ins.trimmingCharacters(in: .whitespaces);
+            let instruction = ins.trimmingCharacters(in: .whitespaces);
             let index = instruction.index(instruction.startIndex, offsetBy: 1);
             let dir: String = instruction.substring(to: index);
             let s = instruction.substring(from: index)
@@ -42,12 +42,9 @@ class Day1 {
             
             if (endPosition.direction == 360) {
                 endPosition.direction = 0;
-            }else if (endPosition.direction == -90){
+            } else if (endPosition.direction == -90){
                 endPosition.direction = 270;
             }
-            
-            
-            
             
             
             switch endPosition.direction {
@@ -59,7 +56,7 @@ class Day1 {
                 endPosition.y -= steps;
             case 270:
                 endPosition.x -= steps;
-
+                
             default:
                 break;
             }
